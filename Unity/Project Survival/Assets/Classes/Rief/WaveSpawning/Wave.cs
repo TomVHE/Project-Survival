ea04e457ceea8befd5f2262d9af9f233638a6f57
Rief Haalboom <superrief@hotmail.com>
2018-03-05 21:26:21 +0100

    movement, camera, wave, spawn

---
+++ Unity/Project Survival/Assets/Classes/Rief/WaveSpawning/Wave.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour {
+
+    private int currWave;
+    public int currEnemy;
+    public int maxEnemy;
+    public GameObject zombie;
+    public List<GameObject> spawnLoc = new List<GameObject>();
+
+	void Start () {
+        maxEnemy = 24;
+	}
+	
+	void Update () {
+        EnemyCheck();
+	}
+
+    void EnemyCheck() {
+
+        if (currEnemy == 0) {
+            StartCoroutine(NextWave());
+            for (int i = 0; i < maxEnemy; i++) {
+                int randomSpawnLoc = Random.Range(0, spawnLoc.Count);
+                Instantiate(zombie, spawnLoc[randomSpawnLoc].transform.position, Quaternion.identity);
+                currEnemy++;
+            }
+        }
+    }
+    IEnumerator NextWave() {
+        print("Next Wave in 5 seconds");
+        yield return new WaitForSeconds(5);
+        print("Wave Starting");
+    }
+}
